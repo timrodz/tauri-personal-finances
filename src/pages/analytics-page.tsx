ec6bdc5ab7ae76c4b94a1eee4709b2682795eaae
Juan Morais <timrodz@icloud.com>
2026-01-31 15:10:12 +1300

    fix: Header on analytics page

--- src/pages/analytics-page.tsx
+++ src/pages/analytics-page.tsx
@@ -1,9 +1,11 @@
+import { Header } from "@/components/header";
 import { PageContainer } from "@/components/page-container";
 import { AnalyticsFeature } from "@/features/analytics/analytics-feature";
 
 export function AnalyticsPage() {
   return (
     <PageContainer>
+      <Header />
       <main>
         <AnalyticsFeature />
       </main>
